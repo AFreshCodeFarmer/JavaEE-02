e63a3f5baa021d2271647aa1ae2fe46ed88ee60f
AFreshCodeFarmer <46767893+AFreshCodeFarmer@users.noreply.github.com>
2020-03-25 01:23:01 +0800

    JavaEE 项目转为 Maven 框架，添加 HikariCP 支持

---
+++ JavaEE-02-1/src/main/java/edu/bjtu/sse/djd/JavaEE/class345/jdbc/StudentJDBC.java
@@ -0,0 +1,129 @@
+package edu.bjtu.sse.djd.JavaEE.class345.jdbc;
+
+import edu.bjtu.sse.djd.JavaEE.class345.DBCP;
+import edu.bjtu.sse.djd.JavaEE.class345.model.Student;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class StudentJDBC {
+
+    // 数据库连接
+    private Connection connection = null;
+    // 执行对象
+    private PreparedStatement pstmt = null;
+
+    // 查找所有记录
+    public List<Student> selectAll() {
+        // 定义 SQL 语句
+        String sql = "SELECT * FROM s_student";
+        System.out.println(sql);
+        // 一条结果
+        Student s = null;
+        // 存储结果集
+        List<Student> resultList = new ArrayList<>();
+
+        try (Connection connection = DBCP.getHikariDataSource().getConnection()){
+            try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                try (ResultSet resultSet = pstmt.executeQuery(sql)) {
+                    // 输出结果集
+                    while (resultSet.next()) {
+                        // 初始化一条结果
+                        s = new Student();
+                        s.setId(resultSet.getLong("id"));
+                        s.setName(resultSet.getString("name"));
+                        s.setCreateTime(resultSet.getTimestamp("create_time"));
+                        s.setUpdateTime(resultSet.getTimestamp("update_time"));
+                        resultList.add(s);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+    }
+
+    // 添加一条记录
+    public void addStudent(Student s) {
+        // 定义 SQL 语句
+        // PreparedStatement
+        String sql = "INSERT INTO s_student(id, name) VALUES (?, ?)";
+
+        try (Connection connection = DBCP.getHikariDataSource().getConnection()){
+            try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setLong(1, s.getId());
+                pstmt.setString(2, s.getName());
+                System.out.println(sql);
+                pstmt.execute();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 删除一条记录
+    public void deleteStudent(Student s) {
+        // 定义 SQL 语句
+        // PreparedStatement
+        String sql = "DELETE FROM s_student WHERE id=?";
+
+        try (Connection connection = DBCP.getHikariDataSource().getConnection()){
+            try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setLong(1, s.getId());
+                System.out.println(sql);
+                pstmt.execute();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 更新一条记录
+    public void updateStudent(Student s) {
+        // 定义 SQL 语句
+        // PreparedStatement
+        String sql = "UPDATE s_student SET name=?, update_time=? WHERE id=?";
+
+        try (Connection connection = DBCP.getHikariDataSource().getConnection()){
+            try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setString(1, s.getName());
+                pstmt.setTimestamp(2, new Timestamp(new Date().getTime()));
+                pstmt.setLong(3, s.getId());
+                System.out.println(sql);
+                pstmt.execute();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static void main(String[] args) {
+//        StudentJDBC sJDBC = new StudentJDBC();
+//        Student s = new Student();
+
+    // To test addStudent(Student s)
+//        s.setId(100L);
+//        s.setName("ddd");
+//        sJDBC.addStudent(s);
+
+    // To test selectAll()
+//        List<Student> list =  sJDBC.selectAll();
+//        for(Student student : list)
+//            System.out.println(student.getName());
+
+    // To test deleteStudent(Student s)
+//        s.setId(100L);
+//        s.setName("ddd");
+//        sJDBC.deleteStudent(s);
+
+    // To test updateStudent(Student s)
+//        s.setId(100L);
+//        s.setName("dd");
+//        sJDBC.updateStudent(s);
+
+//        sJDBC.closeAll();
+//    }
+}
---
+++ JavaEE-02-1/src/main/java/edu/bjtu/sse/djd/JavaEE/class345/servlet/HomeworkStudentServlet.java
@@ -0,0 +1,24 @@
+package edu.bjtu.sse.djd.JavaEE.class345.servlet;
+
+import edu.bjtu.sse.djd.JavaEE.class345.jdbc.HomeworkJDBC;
+import edu.bjtu.sse.djd.JavaEE.class345.model.Homework;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+// 学生看到的作业列表，仅可提交作业
+@WebServlet(name = "HomeworkStudentServlet", urlPatterns = "/HomeworkStudent")
+public class HomeworkStudentServlet extends HttpServlet {
+    private HomeworkJDBC homeworkJDBC = new HomeworkJDBC();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Homework> list = homeworkJDBC.selectAll();
+        req.setAttribute("list",list);
+        req.getRequestDispatcher("/jsp/homework-student.jsp").forward(req, resp);
+    }
+}
